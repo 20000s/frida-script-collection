82955bc3e147c1e7c7bf10cf3b256b90e29cbc6a
20000s <56385387+20000s@users.noreply.github.com>
2023-09-06 10:58:33 +0800

    map update

--- 其他/打印常见数据结构/index.js
+++ 其他/打印常见数据结构/index.js
@@ -4,3 +4,16 @@
         var element = iterator.next();
         console.log(element);
     }
+
+//打印map
+var Map = Java.use('java.util.Map');
+    
+var mapInstance = Java.cast(result, Map);
+
+var keySet = mapInstance.keySet();
+var iterator = keySet.iterator();
+while (iterator.hasNext()) {
+    var key = iterator.next();
+    var value = mapInstance.get(key);
+    console.log('Key:', key, 'Value:', value);
+}
